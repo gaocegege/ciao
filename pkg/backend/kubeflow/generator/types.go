3dbc837bbf36ef61ea6a01f7aa202b0623a5f6cd
Ce Gao <gaoce@caicloud.io>
2018-10-01 12:53:03 +0800

    types: Add comment
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- pkg/backend/kubeflow/generator/types.go
+++ pkg/backend/kubeflow/generator/types.go
@@ -7,6 +7,7 @@ import (
 	"github.com/caicloud/ciao/pkg/types"
 )
 
+// Interface is the type for generator, which is used to generate TFJob/PyTorchJob.
 type Interface interface {
 	GenerateTFJob(parameter *types.Parameter) *tfv1alpha2.TFJob
 	GeneratePyTorchJob(parameter *types.Parameter) *pytorchv1alpha2.PyTorchJob
