c60c3899b3add7e255a4fc97d001b80a57a7ccab
Ce Gao <gaoce@caicloud.io>
2019-07-16 10:21:29 +0800

    WIP
    Signed-off-by: Ce Gao <gaoce@caicloud.io>

--- artifacts/kernel.js
+++ artifacts/kernel.js
@@ -1,9 +1,34 @@
 define(function(){
+    var nb = IPython.notebook;
+    window.CodeCell = require("notebook/js/codecell").CodeCell;
+
     return {onload: function(){
-      console.info('Kernel specific javascript loaded');
-  
-      // do more things here, like define a codemirror mode
+      console.info('load cell');
+
+      events.on("create.Cell", function(evt, param) {
+        add_lan_selector(param.cell);
+      });
   
     }}
+
+    function add_lan_selector(cell) {
+        // add a new one
+        var select = $("<select/>")
+        .attr("id", "cell_kernel_selector")
+        .css("right", "100px")
+        .attr("class", "select-xs cell_kernel_selector");
+        select.append(
+        $("<option/>")
+            .attr("value", "1")
+            .text("1 PS, 2 Worker")
+        );
+        select.append(
+            $("<option/>")
+                .attr("value", "1")
+                .text("1 PS, 4 Worker")
+            );
+        cell.element.find("div.input_area").prepend(select);
+        return select;
+    }
   
   });
